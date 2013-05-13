67e4d53b3b84a96dbebe2e8a20b816ef3c2a30c0
Matth_eu <matthew@matth.eu>
2013-05-13 18:29:32 +0100

    don't add domain to whitelist if already whitelisted

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,6 +18,17 @@ $GLOBALS['wp_tests_options'] = array(
 require dirname( __FILE__ ) . '/lib/bootstrap.php';
 
 class Sideload_Images_UnitTestCase extends WP_UnitTestCase {
+/**
+ * Filter whitelist to make sure github is valid.
+ * @param  array $whitelist
+ * @return array $whitelist
+ */
+function sideload_images_test_whitelist ($whitelist) {
+	if ( ! in_array( 'https://raw.github.com/', $whitelist ) )	
+		array_push( $whitelist, 'https://raw.github.com/' );
+	return $whitelist;
+}
+add_filter( 'hm_sideload_images', 'sideload_images_test_whitelist' );
 
 	/** A referene to the actual plugin that we're testing. */
 	private $plugin;
